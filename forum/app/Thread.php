723a582daaf67003e5f605b560060ea60e49bd8f
Amadeu Cavalcante <amadeu01@gmail.com>
2017-10-14 23:37:55 -0200

    Fixes test

--- forum/app/Thread.php
+++ forum/app/Thread.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    function path(){
+    public function path() {
         return '/threads/' . $this->id;
+    }
+
+    public function replies() {
+        return $this->hasMany(Reply::class);
     }
 }
--- forum/tests/Feature/ThreadsTest.php
+++ forum/tests/Feature/ThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Thread;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -9,27 +10,27 @@ class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
-    protected function setUp()
+    /**
+     * @var Thread
+     */
+    private $thread;
+
+    public function setUp()
     {
         parent::setUp();
+        $this->thread = factory('App\Thread')->create();
     }
 
     /** @test */
-    public function a_user_can_view_all_threads()
+    function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads');
-
-        $response->assertSee($thread->title);
+         $this->get('/threads')
+             ->assertSee($this->thread->title);
     }
 
     /** @test */
-    public function a_user_can_read_a_single_thread() {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads/' . $thread->id);
-
-        $response->assertSee($thread->title);
+    function a_user_can_read_a_single_thread() {
+        $this->get($this->thread->path())
+            ->assertSee($this->thread->title);
     }
 }
